cc567701807d833e6473126da39b6b62fb12e3f3
Nroseman <nicholas.roseman@gmail.com>
2023-09-13 15:51:21 -0700

    refactor quests and add guild gifts

--- player/main.py
+++ player/main.py
@@ -10,11 +10,11 @@ import mss.tools
 from pywinauto.application import Application
 import cv2
 import PIL
-from PIL import Image
+from PIL import Image, ImageGrab
 import pytesseract
 import datetime as dt
 import time
-
+from time import strftime
 
 load_dotenv()
 # path_to_tesseract = os.getenv("PATH_TESSERACT")
@@ -28,7 +28,7 @@ app.connect(handle=target_window)
 window_rect = app.window().child_window().rectangle()
 window_region = (window_rect.left, window_rect.top, window_rect.right -
                  window_rect.left, window_rect.bottom - window_rect.top)
-print(window_region)
+print(window_region)  # must be (70, 31, 1780, 1001)
 
 auto_construct = False
 
@@ -51,6 +51,9 @@ location_box = {
     "height": 85
 }
 
+yellow_rgb = (255, 247, 153)
+red_rgb = (214, 48, 49)
+
 change_screen_loc = (115, 920)
 
 help_loc = (1730, 775)
@@ -58,15 +61,65 @@ helpall_loc = (920, 920)
 
 exit_loc = (1715, 60)
 
+# TODO: are these necessary?
 quest_loc = (1125, 940)
+quest_notification_rgb = (214, 48, 49)
+admin_quest_notification_loc = ()
 admin_quest_loc = (885, 210)
 guild_quest_loc = (1190, 215)
+guild_quest_notification_loc = ()
 quest_complete_loc = (1545, 432)
-quest_start_loc = (1573, 43)
+quest_start_loc = (1568, 426)
 quest_rgb = (255, 247, 153)
+quest_auto_loc = ()
+
+quests = {
+    "admin": {
+        "notification_rgb": red_rgb,
+        "notification_loc": (1014, 167),
+        "tab_loc": (885, 210),
+        "collect_loc": (1577, 391),
+        "start_loc": (1560, 391),
+        "auto_loc": (1625, 400),
+        "quest_rgb": yellow_rgb,
+        "is_auto": True,
+        "total_quests": 7,
+        "timer": 0
+
+    },
+    "guild": {
+        "notification_rgb": red_rgb,
+        "notification_loc": (1332, 167),
+        "tab_loc": (1190, 215),
+        "collect_loc": (1577, 391),
+        "start_loc": (1560, 391),
+        "auto_loc": (1625, 400),
+        "quest_rgb": yellow_rgb,
+        "is_auto": False,
+        "total_quests": 7,
+        "timer": 0
+    }
+}
 
 mystery_box_loc = (1603, 730)
 mystery_claim_loc = (885, 750)
+
+guild_loc = (825, 940)
+guild_gift_rgb = (255, 77, 36)
+guild_tab_loc = (1650, 260)
+guild_gift_loc = (1345, 385)
+bonus_chest_tab_loc = (1420, 320)
+guild_gift_tab_loc = (940, 340)
+open_chest_loc = (1540, 440)
+open_chest_px = (1575, 466)
+open_chest_rgb = (255, 255, 255)
+
+# Are admin/guild quests auto-complete through VIP points
+is_admin_auto = True
+is_guild_auto = False
+
+timer_1 = 0
+timer_5 = 0
 
 
 def send_help():
@@ -74,7 +127,7 @@ def send_help():
     print("Checking if help requested... ", end="")
     if pyautogui.locateOnScreen(
             'assets/help_button.PNG', region=window_region, grayscale=True, confidence=.9):
-        print("Pressing Help... ", end="")
+        print("Pressing help... ", end="")
         app.window().click(
             button='left', coords=help_loc)
         time.sleep(.5)
@@ -83,25 +136,24 @@ def send_help():
         time.sleep(1)
         app.window().click(
             button='left', coords=exit_loc)
-        time.sleep(1)
-        print("DONE")
     else:
-        print("No help detected")
+        print("No help detected", end=" ", flush=True)
+    print("DONE")
 
 
 # This needs work
 def click_queue():
-    print("Checking project queue... ", end="")
+    print("Checking project queue... ", end="", flush=True)
     if pyautogui.locateOnScreen(
             'assets/free_buton.PNG', region=window_region, grayscale=True, confidence=.9):
-        print("Pressing Free... ", end="")
+        print("Pressing Free... ", end="", flush=True)
         app.window().click(
             button='left', coords=(275, 125))
         time.sleep(1)
         # TODO: Need to find a way to differentiate between construction, research, etc.
         # This only works for construction - will break eventually
         if auto_construct:
-            print("Locating next project...")
+            print("Locating next project...", flush=True)
             app.window().click(
                 button='left', coords=(275, 125))
             time.sleep(5)
@@ -127,49 +179,104 @@ def click_queue():
 
 
 def check_quests():
-    print("Checking for quests...", end=" ")
+    print("Checking for quests...", end=" ", flush=True)
     app.window().click(
         button='left', coords=quest_loc)
-    time.sleep(1)
-    for quest in [admin_quest_loc, guild_quest_loc]:
-        app.window().click(
-            button='left', coords=quest)
-        time.sleep(.5)
-        if pyautogui.pixel(quest_complete_loc[0], quest_complete_loc[1]) == quest_rgb:
-            print("Completing quest...", end=" ")
-            app.window().click(
-                button='left', coords=quest_complete_loc)
-            time.sleep(2)
-        if pyautogui.pixel(quest_start_loc[0], quest_start_loc[1]) == quest_rgb:
-            print("Starting quest...", end=" ")
-            app.window().click(
-                button='left', coords=quest_start_loc)
+    time.sleep(2)
+    screen = ImageGrab.grab(bbox=(window_rect.left, window_rect.top,
+                                  window_rect.right, window_rect.bottom))
+    for quest in quests:
+        current = quests[quest]
+        if screen.getpixel(current["notification_loc"])[0] > 200:  # if it's red
+            app.window().click(button='left', coords=current["tab_loc"])
             time.sleep(1)
+            if current["is_auto"] == True:
+                print(f"{quest} are auto complete...", end=" ", flush=True)
+                for x in range(current["total_quests"]):
+                    app.window().click(button='left',
+                                       coords=current["auto_loc"])
+                    time.sleep(.5)
+                time.sleep(1)
+                print("spam clicking is done")
+
+            # Manually start/complete quests
+            # collect button
+            elif screen.getpixel(current["collect_loc"]) == current["quest_rgb"]:
+                print("Starting quest...", end=" ", flush=True)
+                app.window().click(button='left', coords=current["auto_loc"])
+                time.sleep(1)
+            else:  # Start button
+                print("Completing quest and starting next...", end=" ", flush=True)
+                app.window().click(button='left',
+                                   coords=current["auto_loc"])  # click complete
+                time.sleep(1)
+                # click start on next quest
+                app.window().click(button='left', coords=current["auto_loc"])
+                time.sleep(1)
+    app.window().click(button='left', coords=exit_loc)
     print("DONE")
-    app.window().click(
-        button='left', coords=exit_loc)
+    time.sleep(1)
 
 
 def check_box():
-    print("checking if mystery box ready...", end=" ")
-    # - screenshot and pytesseract maybe overly complicated... - #
-    # take_screenshot(box=collect_box)
-    # if read_image() == "Collect":
-
+    print("checking if mystery box ready...", end=" ", flush=True)
     # get the blue value from mystery box counter
     px_b = pyautogui.pixel(1627, 857)[2]
     if px_b >= 187 and px_b <= 193:  # range due to the color changing with background
-        print("clicking mystery box...", end=" ")
+        print("clicking mystery box...", end=" ", flush=True)
         app.window().click(
             button='left', coords=mystery_box_loc)
         time.sleep(1)
-        print("clicking claim...", end=" ")
+        print("clicking claim...", end=" ", flush=True)
         app.window().click(
             button='left', coords=mystery_claim_loc)
         time.sleep(3)
         print("DONE")
     else:
         print("Box not ready")
+
+
+# TODO: Get more data points
+# Does it default to bonus gifts always?
+# multiple gifts?
+def check_guild_gift():
+    print("Checking for guild gifts...", end=" ", flush=True)
+    time.sleep(1)
+    px1 = pyautogui.pixel(845, 945)
+    if px1 == guild_gift_rgb:
+        app.window().click(
+            button='left', coords=guild_loc)
+        time.sleep(1)
+        app.window().click(
+            button='left', coords=guild_tab_loc)
+        time.sleep(1)
+        app.window().click(
+            button='left', coords=guild_gift_loc)
+        time.sleep(1)
+        app.window().click(
+            button='left', coords=guild_tab_loc)
+        time.sleep(1)
+        app.window().click(
+            button='left', coords=bonus_chest_tab_loc)
+        for loc in [bonus_chest_tab_loc, guild_gift_tab_loc]:
+            app.window().click(
+                button='left', coords=loc)
+            px2 = pyautogui.pixel(open_chest_px[0], open_chest_px[1])
+            if px2 == open_chest_rgb:
+                print("Received gift...", end=" ", flush=True)
+                app.window().click(
+                    button='left', coords=open_chest_loc)
+                time.sleep(1)
+                app.window().click(
+                    button='left', coords=open_chest_loc)
+                time.sleep(1)
+        app.window().click(
+            button='left', coords=exit_loc)
+        time.sleep(1)
+        app.window().click(
+            button='left', coords=exit_loc)
+    print("DONE")
+    time.sleep(1)
 
 
 def get_location():
@@ -245,16 +352,41 @@ def read_image():
 # read_image()
 # check_quests()
 # check_box()
-# go_to_location("Castle")
-app.top_window().set_focus()
+# check_guild_gift()
+# pyautogui.displayMousePosition()
+
+# - Main Loop - #
+app.top_window().set_focus()  # Window needs to be visible for pyautogui
 while True:
-    print("New cycle begins")
+    start = time.perf_counter()
+
+    print(f"New cycle begin - {time.strftime('%H:%M:%S')}:")
+
+    last_location = get_location()
+
     if go_to_location("Castle") == None:
         print("Waiting and will try again in 1 minute")
         time.sleep(60)
         continue
-    check_box()
-    send_help()
-    check_quests()
-    print("Cycle Done")
-    time.sleep(300)
+
+    if timer_1 - start <= 0:
+        check_box()
+        timer_1 = start + 60
+
+    if timer_5 - start <= 0:
+        send_help()
+        check_quests()
+        check_guild_gift()
+        timer_5 = start + 300
+
+    go_to_location(last_location)
+
+    print(f"Cycle Done - {time.strftime('%H:%M:%S')}")
+    print("Next cycle in 1 minutes")
+
+    time.sleep(60)
+
+# img = ImageGrab.grab(bbox=(window_rect.left, window_rect.top,
+#                      window_rect.right, window_rect.bottom))
+# px = img.getpixel(quests["guild"]["notification_loc"])
+# print(px)
